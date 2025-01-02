a9f6d1860c07375e18fde1ca4cf39b4f13adca63
Fortissuport <fortissuplmentos@gmail.com>
2025-01-02 02:40:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function home() {
+    const seccion = document.getElementById("home");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
+function buy() {
+    const seccion = document.getElementById("store");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
+function contact() {
+    const seccion = document.getElementById("contact");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
+
+
+function gotoSection1() {
+    const seccion = document.getElementById("product1");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
+function gotoSection2() {
+    const seccion = document.getElementById("product2");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
+function gotoSection3() {
+    const seccion = document.getElementById("product3");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
+function gotoSection4() {
+    const seccion = document.getElementById("product4");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
+function gotoSection5() {
+    const seccion = document.getElementById("product5");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
+function gotoSection6() {
+    const seccion = document.getElementById("product6");
+    seccion.scrollIntoView({ behavior: 'smooth'});
+}
